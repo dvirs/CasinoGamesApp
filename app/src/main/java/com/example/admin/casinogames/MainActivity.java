4b60aae20394d3ed8407c6d80bc762946266c15f
omri <gomri12@gmail.com>
2015-01-07 16:53:45 +0200

    last update 7.1.15 17:00

--- app/src/main/java/com/example/admin/casinogames/MainActivity.java
+++ app/src/main/java/com/example/admin/casinogames/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.admin.casinogames;
 
+import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -10,8 +10,7 @@ import android.widget.Button;
 import android.view.View.OnClickListener;
 
 
-public class MainActivity extends ActionBarActivity {
-
+public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,6 +36,42 @@ public class MainActivity extends ActionBarActivity {
         });
 
     }
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+
+
 
 
     @Override
--- app/src/main/java/com/example/admin/casinogames/RegisterActivity.java
+++ app/src/main/java/com/example/admin/casinogames/RegisterActivity.java
@@ -1,20 +1,96 @@
 package com.example.admin.casinogames;
 
-import android.support.v7.app.ActionBarActivity;
+
+import android.app.Activity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
-public class RegisterActivity extends ActionBarActivity {
+public class RegisterActivity extends Activity {
+    private EditText firstPassword;
+    private EditText secondPassword;
+    private EditText userName;
+    private EditText email;
+    private Button signUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        
+
+        signUp = (Button) findViewById(R.id.singUp_btn_reg);
+        email = (EditText) findViewById(R.id.email_et);
+        firstPassword = (EditText) findViewById(R.id.password_et_reg);
+        secondPassword = (EditText) findViewById(R.id.rePassword_et_reg);
+        userName = (EditText) findViewById(R.id.userName_reg);
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user = userName.getText().toString();
+                String emailStr = email.getText().toString();
+                String firstPass = firstPassword.getText().toString();
+                String secondPass = secondPassword.getText().toString();
+                if(!isValidEmail(emailStr)) {
+                    Toast.makeText(RegisterActivity.this, "Email not Valid", Toast.LENGTH_SHORT).show();
+                    firstPassword.setText("");
+                    secondPassword.setText("");
+                }
+                if(!firstPass.equals(secondPass) || firstPass.equals("")){
+                    Toast.makeText(RegisterActivity.this,"The Passwords Dont Match",Toast.LENGTH_LONG).show();
+                    firstPassword.setText("");
+                    secondPassword.setText("");
+
+                }else{
+                    Toast.makeText(RegisterActivity.this,"Loading...",Toast.LENGTH_LONG).show();
+                    signUp.setClickable(false);
+                    signUp.setFocusable(false);
+                }
+            }
+        });
+
+
+    }
+    private static boolean isValidEmail(CharSequence target) {
+        return !TextUtils.isEmpty(target) && android.util.Patterns.EMAIL_ADDRESS.matcher(target).matches();
     }
 
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
